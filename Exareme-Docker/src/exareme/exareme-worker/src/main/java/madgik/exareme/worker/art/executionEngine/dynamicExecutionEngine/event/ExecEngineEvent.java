24cfa1d2bd795e1a1fec858ad4eec3704af0c38b
ThanKarab <tkarabatsis@hotmail.com>
2020-05-11 21:12:25 +0300

    Debugging.

--- Exareme-Docker/src/exareme/exareme-worker/src/main/java/madgik/exareme/worker/art/executionEngine/dynamicExecutionEngine/event/ExecEngineEvent.java
+++ Exareme-Docker/src/exareme/exareme-worker/src/main/java/madgik/exareme/worker/art/executionEngine/dynamicExecutionEngine/event/ExecEngineEvent.java
@@ -5,6 +5,8 @@ package madgik.exareme.worker.art.executionEngine.dynamicExecutionEngine.event;
 
 import madgik.exareme.utils.eventProcessor.Event;
 import madgik.exareme.worker.art.executionEngine.dynamicExecutionEngine.PlanEventSchedulerState;
+import madgik.exareme.worker.art.executionEngine.dynamicExecutionEngine.event.independent.IndependentEventsHandler;
+import org.apache.log4j.Logger;
 
 import java.util.concurrent.Semaphore;
 
@@ -15,12 +17,15 @@ public abstract class ExecEngineEvent implements Event {
     public final PlanEventSchedulerState state;
     public Semaphore wait = null;
 
+    private static final Logger logger = Logger.getLogger(ExecEngineEvent.class);
     public ExecEngineEvent(PlanEventSchedulerState state) {
         this.state = state;
     }
 
     public void done() {
+        logger.debug("Event Done!");
         if (wait != null) {
+            logger.debug("Releasing.");
             wait.release();
         }
     }
